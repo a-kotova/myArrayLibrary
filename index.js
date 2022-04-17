ee598c714d15ebdf5b2c450eda8ceb012772caa8
Anna Kotova <a.annykot@gmail.com>
2022-04-17 16:49:58 +0200

    Update skip method to use ES6

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const chapter = (function() {
-  const skip = function(array, n) {
+class Chapter {
+  skip(array, n) {
     const output = array.slice();
     output.splice(0, n);
     return output;
   }
-
-  return {
-    skip
-  }
-})();
+}
