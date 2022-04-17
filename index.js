da059e94de5d06b7d465f5191e3a6637d33357e9
Anna Kotova <a.annykot@gmail.com>
2022-04-17 16:51:38 +0200

    Update reduce method to use ES6

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const chapter = (function() {
-  const reduce = function(array, callback, initialValue = null) {
+class Chapter {
+  reduce(array, callback, initialValue = null) {
     let accumulator = initialValue;
     for (let i = 0; i < array.length; i++) {
       accumulator = callback(accumulator, array[i]);
     }
     return accumulator;
   }
-
-  return {
-    reduce
-  }
-})();
+}
