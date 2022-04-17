8f3651ba2e9ec95dc76cef6ca18ee95089b797af
Anna Kotova <a.annykot@gmail.com>
2022-04-17 16:55:24 +0200

    Update map method to use ES6

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const chapter = (function() {
-  const map = function(array, callback) {
+class Chapter {
+  map(array, callback) {
     const output = [];
     for (let i = 0; i < array.length; i++) {
       output.push(callback(array[i]));
     }
     return output;
   }
-
-  return {
-    map
-  }
-})();
+}
