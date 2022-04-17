f6a070bd89e190c7901416576fc5fd19d542018c
Anna Kotova <a.annykot@gmail.com>
2022-04-17 16:47:52 +0200

    Update filter method to ES6

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const chapter = (function() {
-  const filter = function(array, callback) {
+class Chapter {
+  filter(array, callback) {
     const output = [];
     for (let i = 0; i < array.length; i++) {
       if (callback(array[i])) output.push(array[i]);
     }
     return output;
   }
-
-  return {
-    filter
-  }
-})();
+}
