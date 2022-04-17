332864e1607ae278243d90bb0dc35306aec8d0bf
Anna Kotova <a.annykot@gmail.com>
2022-04-17 16:53:52 +0200

    Update forEach method to use ES6

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const chapter = (function() {
-  const forEach = function(array, callback) {
+class Chapter {
+  forEach(array, callback) {
     for (let i = 0; i < array.length; i++) {
       callback(array[i]);
     }
   }
-
-  return {
-    forEach
-  }
-})();
+}
