60e57f04fc6b99ec3711c56fff574fe3a11ff118
Anna Kotova <a.annykot@gmail.com>
2022-04-25 16:51:32 +0200

    Implement chain method, change map, skip, take methods

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 class Chapter {
+  constructor() {
+    this.result = null;
+  }
+
   filter(array, callback) {
     const output = [];
     for (let i = 0; i < array.length; i++) {
@@ -18,10 +22,22 @@ class Chapter {
 
   map(array, callback) {
     const output = [];
-    for (let i = 0; i < array.length; i++) {
-      output.push(callback(array[i]));
+
+    if (typeof array === 'function') {
+      for (let i = 0; i < this.result.length; i++) {
+        output.push(array(this.result[i]));
+      }
+
+      this.result = output;
+
+      return this;
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        output.push(callback(array[i]));
+      }
+
+      return output;
     }
-    return output;
   }
 
   forEach(array, callback) {
@@ -31,13 +47,32 @@ class Chapter {
   }
 
   skip(array, n) {
-    const output = array.slice();
-    output.splice(0, n);
-    return output;
+    if (typeof array === 'number') {
+      const output = this.result.slice();
+
+      output.splice(0, array);
+      this.result = output;
+      return this;
+
+    } else {
+      const output = array.slice();
+
+      output.splice(0, n);
+      return output;
+    }
   }
 
   take(array, n) {
+    if (typeof array === 'number') {
+      this.result = this.result.slice(0, array);
+      return this;
+    }
     return array.slice(0, n);
+  }
+
+  chain(array) {
+    this.result = array;
+    return this;
   }
 }
 
